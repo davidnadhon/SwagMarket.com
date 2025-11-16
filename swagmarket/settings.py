216cbc795e567a1fcbce4beb0b736c12392ae6a0
carson444 <ahmmalogbone@gmail.com>
2025-11-16 19:37:09 +0000

    comite_start
    grace a ce comite nous demmarons notre projet avec d'accueill que nous modeliseon au fur  et a mesur de la progresion su projet  et courage

--- swagmarket/settings.py
+++ swagmarket/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accueil',
 ]
 
 MIDDLEWARE = [
